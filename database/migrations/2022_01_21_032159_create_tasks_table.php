6d34d652c546470bac4e404fa72f87d7573c7229
nblkmal <nblkmal@gmail.com>
2022-01-21 11:26:56 +0800

    add migrations & model

---
+++ database/migrations/2022_01_21_032159_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('due_date')->nullable();
+            $table->string('due_time')->nullable();
+            $table->string('status')->nullable();
+
+            $table->unsignedInteger('workspace_id')->nullable();
+            $table->foreign('workspace_id')->references('id')->on('workspaces');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
