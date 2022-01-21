1c44e9d758742ef589d9f5bad63f2851f89dd75c
nblkmal <nblkmal@gmail.com>
2022-01-21 17:27:17 +0800

    workspace : add relationship

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,6 +18,11 @@ class Workspace extends Model
         'description',
         'user_id'
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 
     public function user()
     {
