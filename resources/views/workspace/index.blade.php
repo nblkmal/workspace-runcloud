3d9008da25223ec19976039cdea5de243c9e5e47
nblkmal <nblkmal@gmail.com>
2022-01-22 02:25:27 +0800

    workspace : update ui

--- resources/views/workspace/index.blade.php
+++ resources/views/workspace/index.blade.php
@@ -2,41 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 card-white">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($spaces as $work)
-                            <tr>
-                                <th scope="row">{{ $loop->iteration }}</th>
-                                <td>{{ $work->name }}</td>
-                                <td>{{ $work->description }}</td>
-                                <td>
-                                    <a href="{{ route('workspace:show', $work) }}">More</a>
-                                </td>
-                            </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    @if ($spaces->isEmpty())
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-white">
+                <h3>You dont have any workspace. Create one now!</h3>
+            </div>
         </div>
-    </div>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" type="button" data-bs-toggle="modal" data-bs-target="#workspace">
-                <div class="card-body text-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
-                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
-                    </svg>
-                    Create a workspace
-                </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-white" type="button" data-bs-toggle="modal" data-bs-target="#workspace">
+                Create a workspace
             </div>
 
             <div class="modal fade" id="workspace" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -66,6 +41,66 @@
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-white">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($spaces as $work)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td>{{ $work->name }}</td>
+                                    <td>{{ $work->description }}</td>
+                                    <td>
+                                        <a href="{{ route('workspace:show', $work) }}">More</a>
+                                    </td>
+                                </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-grey" type="button" data-bs-toggle="modal" data-bs-target="#workspace">
+                Create new workspace
+            </div>
+
+            <div class="modal fade" id="workspace" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <form action="{{ route('workspace:create') }}" method="post">
+                            @csrf
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Create new workspace</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Workspace Name</label>
+                                    <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Workspace Description</label>
+                                    <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+    
 </div>
 @endsection
--- resources/views/workspace/show.blade.php
+++ resources/views/workspace/show.blade.php
@@ -8,84 +8,98 @@
         </div>
     </div>
 
-    <div class="row justify-content-center my-2">
-        <div class="col-md-8 card-white">
-            <h3>List of tasks</h3>
+    @if ($tasks->isEmpty())
+        <div class="row justify-content-center my-2">
+            <div class="col-md-8">
+                <div class="alert alert-primary text-center" role="alert">
+                    You dont have any task yet! <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#task">Create Task</button>
+                </div>
+            </div>
+        </div>
 
-            <h5>Task Completed</h5>
-            <table class="table">
-                <tbody>
-                    @forelse ($complete_tasks as $task)
-                        <tr>
-                            <div class="alert alert-success" role="alert">
-                                {{ $task->name }}
-                                <div class="float-right">
-                                    Completed {{ $task->task_complete->diffForHumans() }}
+    @else
+        <div class="row justify-content-center my-2">
+            <div class="col-md-8 card-white">
+                <h3>List of tasks</h3>
 
+                <h5>Task Completed {{ $complete_tasks->count() }}/{{ $tasks->count() }}</h5>
+                <table class="table">
+                    <tbody>
+                        @forelse ($complete_tasks as $task)
+                            <tr>
+                                <div class="alert alert-success" role="alert">
+                                    {{ $task->name }}
+                                    <div class="float-right">
+                                        Completed {{ $task->task_complete->diffForHumans() }}
+
+                                    </div>
                                 </div>
-                            </div>
-                        </tr>
-                    @empty
-                        <tr>
-                            <div class="alert alert-secondary" role="alert">
-                                No completed task
-                            </div>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+                            </tr>
+                        @empty
+                            <tr>
+                                <div class="alert alert-secondary" role="alert">
+                                    {{ $complete_tasks->count() }}/{{ $tasks->count() }} task completed
+                                </div>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
 
-            <h5>Task Pending</h5>
-            <table class="table">
-                <tbody>
-                    @foreach ($incomplete_tasks as $task)
-                        <tr>
-                            <th scope="row">{{ $loop->iteration }}</th>
-                            <td>{{ $task->name }}</td>
-                            <td>Due on {{ $task->task_due->format('d M Y') }} at {{ $task->task_due->format('H:i a') }}</td>
-                            <td>{{ $task->task_due->diffForHumans() }}</td>
-                            <td>
-                                <a href="{{ route('tasks:update', $task) }}" class="btn btn-success">Complete</a>
-                                {{-- <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> --}}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                <h5>Task Pending</h5>
+                <table class="table">
+                    <tbody>
+                        @foreach ($incomplete_tasks as $task)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td>{{ $task->name }}</td>
+                                <td>Due on {{ $task->task_due->format('d M Y') }} at {{ $task->task_due->format('H:i a') }}</td>
+                                <td>{{ $task->task_due->diffForHumans() }}</td>
+                                <td>
+                                    <a href="{{ route('tasks:update', $task) }}" class="btn btn-success">Complete</a>
+                                    {{-- <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> --}}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="alert alert-primary text-center" role="alert">
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#task">Add Task</button>
+                </div>
+            </div>
+        </div>
+    @endif
+    <div class="modal fade" id="task" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('tasks:create', $workspace) }}" method="post">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="name@example.com">
+                            <label for="name">Task's name</label>
+                        </div>
+                        <div class="row my-2">
+                            <div class="col">
+                                <label for="due_date">Due Date</label>
+                                <input type="date" class="form-control" id="due_date" name="due_date" placeholder="First name" aria-label="First name">
+                            </div>
+                            <div class="col">
+                                <label for="due_time">Due Time</label>
+                                <input type="time" class="form-control" id="due_time" name="due_time" placeholder="Last name" aria-label="Last name">
+                            </div>
+                        </div>
+                        <div class="row my-4 px-2">
+                            <button class="btn btn-primary btn-block" type="submit">Add task</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class="row justify-content-center my-2">
-        <div class="col-md-8 card-white">
-            <form action="{{ route('tasks:create', $workspace) }}" method="post">
-                @csrf
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="name@example.com">
-                    <label for="name">Task's name</label>
-                </div>
-                <div class="row my-2">
-                    <div class="col">
-                        <label for="due_date">Due Date</label>
-                        <input type="date" class="form-control" id="due_date" name="due_date" placeholder="First name" aria-label="First name">
-                    </div>
-                    <div class="col">
-                        <label for="due_time">Due Time</label>
-                        <input type="time" class="form-control" id="due_time" name="due_time" placeholder="Last name" aria-label="Last name">
-                    </div>
-                </div>
-                <div class="row my-4 px-5">
-                    <button class="btn btn-primary btn-block" type="submit">Add task</button>
-                </div>
-            </form>
-            
-        </div>
-    </div>
-
-    {{-- <div class="row">
-        <div class="col-md-8">
-            <input type="text" class="form-control datetimepicker" name="Appointment_time"> 
-        </div>
-    </div> --}}
 </div>
 @endsection
 
