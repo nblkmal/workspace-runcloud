f9757568a4069e3f83d864f6cf4916a6cfd8df9e
nblkmal <nblkmal@gmail.com>
2022-01-22 14:53:34 +0800

    task : add index

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Pending Tasks</h1>
+        </div>
+    </div>
+
+    @if ($tasks->isEmpty())
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-white">
+                <h3>You dont have any workspace. Create one now!</h3>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8 card-white" type="button" data-bs-toggle="modal" data-bs-target="#workspace">
+                Create a workspace
+            </div>
+        </div>
+    @else
+        
+        @foreach ($tasks as $task)
+            <div class="row justify-content-center">
+                <div class="col-md-8 row card-white">
+                    <div class="col-md-1">{{ $loop->iteration }}.</div>
+                    <div class="col">{{ $task->name }}</div>
+                    <div class="col">
+                        <a href="{{ route('workspace:show', $task->workspace_id) }}" class="btn btn-primary">Go to</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        
+    @endif
+</div>
+@endsection
