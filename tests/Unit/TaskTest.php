81345237fe09292281211442cceac421f87bb3b0
nblkmal <nblkmal@gmail.com>
2022-01-24 17:09:33 +0800

    add TaskTest

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -2,17 +2,50 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Carbon\Carbon;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Workspace;
+use Illuminate\Http\Request;
+use App\Http\Controllers\TaskController;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
+    use RefreshDatabase;
+    /** @test */
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_create_task_successfully()
     {
-        $this->assertTrue(true);
+        // $this->withoutExceptionHandling();
+        
+        // create user
+        $user = User::factory()->create([
+            'name' => 'testuser',
+            'email' => 'testuser@gmail.com',
+            'password' => '12345',
+        ]);
+
+        // create workspace
+        $workspace = Workspace::factory()->create();
+
+        $this->actingAs($user);
+
+        $request = Request::create('/workspace/create', 'POST', [
+            'name' => 'tasktest',
+            'due_date' => '2022-01-26',
+            'due_time' => '16:59:00',
+        ]);
+        
+        $controller = new TaskController();
+        $result = $controller->create($request, $workspace);
+
+        $this->assertEquals(302, $result->getStatusCode());
     }
 }
