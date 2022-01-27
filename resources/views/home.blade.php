ec392f92f100d390bb3bb1f6ae5a99b24630812b
nblkmal <nblkmal@gmail.com>
2022-01-28 01:00:44 +0800

    add href to card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,13 +24,20 @@
 
     <div class="row justify-content-center">
         <div class="col-md-8 row">
+            
             <div class="col mx-2 card-white bg-secondary bg-opacity-10 text-center">
-                <div class="rounded-circle">{{ $workspaces }}</div>
-                Total Workspaces
+                <a href="{{ route('workspace:index') }}" style="text-decoration:none; color:black;">
+                    <div class="rounded-circle">{{ $workspaces }}</div>
+                    Total Workspaces
+                </a>
+                
             </div>
             <div class="col mx-2 card-white bg-warning bg-opacity-10 text-center">
-                <div class="rounded-circle">{{ $incomplete_tasks }}</div>
-                Pending Tasks
+                <a href="{{ route('task:index') }}" style="text-decoration:none; color:black;">
+                    <div class="rounded-circle">{{ $incomplete_tasks }}</div>
+                    Pending Tasks
+                </a>
+                
             </div>
             <div class="col mx-2 card-white bg-success bg-opacity-10 text-center">
                 <div class="rounded-circle">{{ $complete_tasks }}</div>
